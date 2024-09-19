93a110daa0cf71075804ba53dcf846efd76270c1
MorenoReyhan <128565990+MorenoReyhan@users.noreply.github.com>
2024-09-19 16:58:57 +0700

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,6 +0,0 @@
-                #elif kedalaman >= 1 and kedalaman < 2:
-                    #tipe_koefisien = 'galian_2m'
-                #else:
-                    #tipe_koefisien = 'galian_1m'
-
-                    python -m streamlit run jalan_paving.py
